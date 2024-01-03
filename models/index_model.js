40f68d792d5233c66969c2906ab5c329f6dde114
berninup <chris.basham@live.com>
2024-01-03 13:15:31 -0600

    corrected table columns

--- models/index_model.js
+++ models/index_model.js
@@ -15,21 +15,14 @@ Photo.belongsTo(User)
 User.hasMany(Travel_Route)
 Travel_Route.belongsTo(User)
 
-User.hasMany(Comment, { foreignKey: 'user_id' })
-Blog_Post.hasMany(Comment, { foreignKey: 'blog_post_id' })
+User.hasMany(Comment)
+Blog_Post.hasMany(Comment)
 
-Comment.belongsTo(User, { foreignKey: 'user_id '})
-Comment.belongsTo(Blog_Post, { foreignKey: 'blog_post_id '})
+Comment.belongsTo(User)
+Comment.belongsTo(Blog_Post)
 
-Photo.belongsTo(Blog_Post, {
-    foreignKey: 'blog_post_id',
-    as: 'blog_post_model',
-    allowNull: true
-})
-Blog_Post.hasMany(Photo, {
-    foreignKey: 'blog_post_id',
-    as: "photo_model"
-})
+Photo.belongsTo(Blog_Post)
+Blog_Post.hasMany(Photo)
 
 module.exports = {
     User,
