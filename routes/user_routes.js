f92d18c10907c5a955c2a974f997da800696089f
berninup <chris.basham@live.com>
2024-01-03 15:45:17 -0600

    Created profile route

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,26 +1,65 @@
-const express = require('express')
-const router = express.Router()
-const User = require('../models/user_model')
-const passport = require('passport')
-const bcrypt = require('bcrypt')
+const express = require("express");
+const router = express.Router();
+const User = require("../models/user_model");
+const passport = require("passport");
+const bcrypt = require("bcrypt");
 
-router.post('/login', passport.authenticate('local', {
-    successRedirect: '/',
-    failureRedirect: '/login',
-    failureFlash: true
-}))
+router.post(
+  "/login",
+  passport.authenticate("local", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+    failureFlash: true,
+  })
+);
 
-router.post('/create', async (req, res) => {
-    try {
-        const { username, email, password } = req.body;
+router.post("/create", async (req, res) => {
+  try {
+    const { username, email, password } = req.body;
 
-        const hashedPassword = await bcrypt.hash(password, 10)
+    const hashedPassword = await bcrypt.hash(password, 10);
 
-        const newUser = await User.create({ username, email, password: hashedPassword })
-        res.status(201).json(newUser)
-    } catch (error) {
-        res.status(400).json({ error: error.message })
+    const newUser = await User.create({
+      username,
+      email,
+      password: hashedPassword,
+    });
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.get("/logout", (req, res) => {
+  req.logout(function (err) {
+    if (err) {
+      return next(err);
     }
-})
+    res.json({ message: "You have been successfully logged out." });
+  });
+});
 
-module.exports = router
+router.get("/profile", (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(403).json({ message: "Not authenticated" });
+  }
+
+  const userId = req.session.passport.user;
+
+  User.findByPk(userId)
+    .then((user) => {
+      if (!user) {
+        return res.status(404).json({ message: "User not found" });
+      }
+
+      const userProfile = {
+        username: user.username,
+        email: user.email,
+      };
+      res.json(userProfile);
+    })
+    .catch((error) => {
+      res.status(500).json({ error: error.message });
+    });
+});
+module.exports = router;
