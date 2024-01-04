ee347c904771b3b3b1fe01854807362431b369dc
berninup <chris.basham@live.com>
2024-01-04 11:49:19 -0600

    React config fixed, React boiler code created

---
+++ client/src/App.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function App() {
+    return (
+        <p>Stuff</p>
+    )
+}
+
+export default App
--- routes/blog_routes.js
+++ routes/blog_routes.js
@@ -1,84 +1,84 @@
-const express = require('express')
-const router = express.Router()
-const { Blog_Post } = require('../models/index_model')
+const express = require("express");
+const router = express.Router();
+const { Blog_Post } = require("../models/index_model");
 
+router.post("/create", async (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: "User not authenticated" });
+  }
 
-router.post('/create', async (req, res) => {
-    if (!req.isAuthenticated()) {
-        return res.status(401).json({ message: 'User not authenticated'})
+  try {
+    const { title, content } = req.body;
+    const userId = req.user.id;
+
+    const newPost = await Blog_Post.create({
+      title: title,
+      content: content,
+      userId: userId,
+    });
+
+    res.status(201).json(newPost);
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
+});
+
+router.put("/update/:postId", async (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: "User is not authenticated" });
+  }
+
+  try {
+    const { title, content } = req.body;
+    const postId = req.params.postId;
+    const userId = req.user.id;
+
+    const post = await Blog_Post.findOne({
+      where: { id: postId, userId: userId },
+    });
+
+    if (!post) {
+      return res
+        .status(404)
+        .json({ message: "Blog post not found or not owned by user" });
     }
 
-    try {
-        const { title, content } = req.body
-        const userId = req.user.id
+    post.title = title;
+    post.content = content;
+    await post.save();
 
-        const newPost = await Blog_Post.create({
-            title: title,
-            content: content,
-            userId: userId
-        })
+    res.json({ message: "Blog post udated successfully", post });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-        res.status(201).json(newPost)        
-    } catch (error) {
-        res.status(400).json({ error: error.message })
-    }
-})
+router.get("/posts", async (req, res) => {
+  if (!req.isAuthenticated()) {
+    return res.status(401).json({ message: "User not authenticated" });
+  }
 
+  try {
+    const userId = req.session.passport.user;
 
-router.put('/update/:postId', async (req, res) => {
-    if (!req.isAuthenticated()) {
-        return res.status(401).json({ message: 'User is not authenticated' })        
-    }
+    const blogPosts = await Blog_Post.findAll({
+      where: { userId: userId },
+    });
+    res.json(blogPosts);
+  } catch (error) {
+    res.status(500).json({ error: error });
+  }
+});
 
-    try {
-        const { title, content } = req.body
-        const postId = req.params.postId
-        const userId = req.user.id
+router.get("/posts/:blogPostId", async (req, res) => {
+  try {
+    const blogPostId = req.params.blogPostId;
+    const blogPost = await Blog_Post.findByPk(blogPostId);
 
-        const post = await Blog_Post.findOne({ where: { id: postId, userId: userId } })
+    res.json(blogPost);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+});
 
-        if (!post) {
-            return res.status(404).json({ message: 'Blog post not found or not owned by user' })
-        }
-
-        post.title = title
-        post.content = content
-        await post.save()
-
-        res.json({ message: 'Blog post udated successfully', post })
-    } catch (error) {
-        res.status(500).json({ error: error.message })
-    }
-})
-
-router.get('/posts', async (req, res) => {
-    if (!req.isAuthenticated()) {
-        return res.status(401).json({ message: 'User not authenticated' })
-    }
-
-    try {
-        const userId = req.session.passport.user        
-
-        const blogPosts = await Blog_Post.findAll({
-            where: { userId: userId }
-        })
-        res.json(blogPosts)
-    } catch (error) {
-        res.status(500).json({ error: error })
-    }
-})
-
-router.get('/posts/:blogPostId' , async (req, res) => {
-    try {
-        const blogPostId = req.params.blogPostId
-        const blogPost = await Blog_Post.findByPk(blogPostId)
-
-        res.json(blogPost)
-    } catch(error) {
-        res.status(500).json({ error: error.message })
-    }
-})
-
-
-
-module.exports = router
+module.exports = router;
