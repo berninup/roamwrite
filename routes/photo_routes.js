d86249e4afe1a6ee4a6997693362e9ea02de0353
berninup <chris.basham@live.com>
2024-01-04 10:02:33 -0600

    Added all photos by logged in user, and find photo by id

--- routes/photo_routes.js
+++ routes/photo_routes.js
@@ -21,4 +21,33 @@ router.post('/submit', async (req, res) => {
     }
 })
 
+router.get('/gallery', async (req, res) => {
+    if (!req.isAuthenticated()) {
+        return res.status(401).json({ message: 'User not authenticated'})        
+    }
+    
+    try {
+        const userId = req.session.passport.user
+
+        const userPhotos = await Photo.findAll({
+            where: { userId: userId }
+        })
+
+        res.json(userPhotos)
+    } catch (error) {
+        res.status(500).json({ error: error.message })
+    }
+})
+
+router.get('/:photoId', async (req, res) => {
+    try {
+        const photoId = req.params.photoId
+        const photo = await Photo.findByPk(photoId)
+        
+        res.json(photo)
+    } catch(error) {
+        res.status(500).json({ error: error.message})
+    }
+})
+
 module.exports = router
