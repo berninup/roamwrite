1fd7a6b2dadc7396677e90c1ed89f190e220e9bb
berninup <chris.basham@live.com>
2024-01-03 23:29:22 -0600

    updated photo model to include user id

--- models/photo_model.js
+++ models/photo_model.js
@@ -1,28 +1,38 @@
-const { Model, DataTypes } = require('sequelize');
-const sequelize = require('../db');
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../db");
 
 class Photo extends Model {}
 
-Photo.init({
-    
+Photo.init(
+  {
     blogPostId: {
-        type: DataTypes.INTEGER,
-        allowNull: true, 
-        references: {
-            model: 'blog_posts', 
-            key: 'id' 
-        }
+      type: DataTypes.INTEGER,
+      allowNull: true,
+      references: {
+        model: "blog_posts",
+        key: "id",
+      },
+    },
+    userId: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "users",
+        key: "id",
+      },
     },
     url: {
-        type: DataTypes.STRING,
-        allowNull: false
+      type: DataTypes.STRING,
+      allowNull: false,
     },
     description: {
-        type: DataTypes.STRING        
-    }    
-}, {
+      type: DataTypes.STRING,
+    },
+  },
+  {
     sequelize,
-    modelName: 'photo',     
-});
+    modelName: "photo",
+  }
+);
 
 module.exports = Photo;
